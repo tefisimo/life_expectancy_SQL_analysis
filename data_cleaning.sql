d0913368093fc3686fe4cd101f68cf531cd14f87
Gustavo Gonzalez <tefisimo@gmail.com>
2025-02-10 20:32:11 -0400

    Data Cleaning: Fill blank 'Life expectancy' values with average of adjacent years

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -87,3 +87,38 @@ SET t1.Status = 'Developed'
 WHERE t1.Status = ''
 AND t2.Status = 'Developed';
 
+-- Looking for more issues
+SELECT *
+FROM life_expectancy
+WHERE `Life expectancy` = '';
+
+-- Calculating the average for these blank values
+SELECT 
+	t1.Country, t1.Year, t1.`Life expectancy`,
+    t2.Country, t2.Year, t2.`Life expectancy`,
+    t3.Country, t3.Year, t3.`Life expectancy`,
+    ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2,1) AS Average_Life_Expectancy
+FROM life_expectancy t1
+JOIN life_expectancy t2
+	ON t1.Country = t2.Country
+	AND t1.Year = t2.Year - 1
+JOIN life_expectancy t3
+	ON t1.Country = t3.Country
+	AND t1.Year = t3.Year + 1
+WHERE t1.`Life expectancy` = '';
+
+-- We can fill the blank values in 'Life expectancy' column with an average of the previous and next value for that country
+UPDATE life_expectancy t1
+JOIN life_expectancy t2
+	ON t1.Country = t2.Country
+	AND t1.Year = t2.Year - 1
+JOIN life_expectancy t3
+	ON t1.Country = t3.Country
+	AND t1.Year = t3.Year + 1
+SET t1.`Life expectancy` = ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2,1)
+WHERE t1.`Life expectancy` = '';
+
+-- Reviewing the update
+SELECT *
+FROM life_expectancy
+WHERE `Life expectancy` = '';
